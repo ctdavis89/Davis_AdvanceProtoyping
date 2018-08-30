f517cb67b79ce034a07e1ba81b1788284b4d43d4
ctdavis89 <36212638+ctdavis89@users.noreply.github.com>
2018-08-30 10:14:46 -0800

    pushing

---
+++ Davis_advancePrototyping/Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float mSpeed;
+    private Vector3 spawn;
+
+    // Use this for initialization
+    void Start()
+    {
+        spawn = transform.position;
+        mSpeed = 5f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(mSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, mSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+}
