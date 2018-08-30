412bde8e656420d8c8604bded1290b19c93fdc86
ctdavis89 <36212638+ctdavis89@users.noreply.github.com>
2018-08-30 11:34:33 -0800

    updates

---
+++ Davis_advancePrototyping/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+    public GameObject YouWin;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != null)
+            Destroy(gameObject);
+    }
+
+    public void Win()
+    {
+        YouWin.SetActive(true);
+        // slow down time
+        //reset the game
+    }
+}
---
+++ Davis_advancePrototyping/Assets/Scripts/Goal.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    void OntriggerEnter()
+    {
+        GameManager.instance.Win();
+
+    }
+}
