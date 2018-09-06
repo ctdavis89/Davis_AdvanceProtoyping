626c2ee9da4e70f919d1280e6b03f599ba808f4d
Student <Student@aii.stu>
2018-09-06 18:19:06 -0400

    Updates
    made a Camera Control that rotates around with the mouse around player and I made a Timer the only thing missing is the reverse controls

---
+++ Davis_advancePrototyping/Assets/Scripts/CoolDownScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoolDownScript : MonoBehaviour {
+
+    [SerializeField] private Text uiText;
+    [SerializeField] private float mainTimer;
+
+    private float timer;
+    private bool canCount = true;
+    private bool doOnce = false;
+
+    // Use this for initialization
+    void Start () {
+
+        timer = mainTimer;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if (timer >= 0.0f && canCount)
+        {
+            timer -= Time.deltaTime;
+            uiText.text = timer.ToString("F");
+
+        }
+
+        else if (timer <= 0.0f && !doOnce)
+        {
+            canCount = false;
+            doOnce = true;
+            uiText.text = "0.00";
+            timer = 0.0f;
+        }
+
+	}
+}
---
+++ Davis_advancePrototyping/Assets/Scripts/PlayerFollow.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour {
+
+    public Transform PLayerTransform;
+    private Vector3 _cameraOffset;
+
+    [Range(0.0f, 1.0f)]
+    public float SmoothFactor = 0.5f;
+    public bool LookAtPlayer = false;
+
+    public bool RotateAroundPlayer = true;
+
+    public float RotationsSpeed = 0.5f;
+
+
+	// Use this for initialization
+	void Start () {
+
+        _cameraOffset = transform.position - PLayerTransform.position;
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(RotateAroundPlayer)
+        {
+            Quaternion camTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * RotationsSpeed, Vector3.up);
+            _cameraOffset = camTurnAngle * _cameraOffset;
+        }
+
+        Vector3 newPos = PLayerTransform.position + _cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+
+        if (LookAtPlayer || RotateAroundPlayer)
+            transform.LookAt(PLayerTransform);
+
+	}
+}
