5149d73a20282b2c16a9db18922d06844bb2f6de
chris Davis <36212638+ctdavis89@users.noreply.github.com>
2018-09-20 18:21:36 -0400

    controls finally work

--- Davis_advancePrototyping/Assets/Scripts/NewGameControls/Movement.cs
+++ Davis_advancePrototyping/Assets/Scripts/NewGameControls/Movement.cs
@@ -9,30 +9,33 @@ public class Movement : MonoBehaviour
     public float mSpeed;
     private Vector3 spawn;
 
+    bool _useSchemeOne;
+
     // Use this for initialization
     void Start()
     {
         spawn = transform.position;
         mSpeed = 5f;
 
+        StartCoroutine(Scheme1());
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(mSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, mSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+        if(_useSchemeOne)
+            transform.Translate(mSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, mSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+        else
+            transform.Translate(mSpeed * Input.GetAxis("Vertical") * Time.deltaTime, 0f, mSpeed * Input.GetAxis("Horizontal") * Time.deltaTime);
     }
-
-
-
 
 
     IEnumerator Scheme1()
     {
         yield return new WaitForSecondsRealtime(20);
 
+        _useSchemeOne = true;
         StartCoroutine(Scheme2());
-        StopCoroutine(Scheme1());
     }
 
 
@@ -40,9 +43,7 @@ public class Movement : MonoBehaviour
     {
         yield return new WaitForSecondsRealtime(20);
 
+        _useSchemeOne = false;
         StartCoroutine(Scheme1());
-       
-        StopCoroutine(Scheme2());
-
     }
 }
