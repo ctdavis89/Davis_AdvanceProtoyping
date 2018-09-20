00c897be23b595ac034c38f780235bb02f142a8b
chris Davis <36212638+ctdavis89@users.noreply.github.com>
2018-09-20 16:43:20 -0400

    lol

--- Davis_advancePrototyping/Assets/Scripts/NewGameControls/Movement.cs
+++ Davis_advancePrototyping/Assets/Scripts/NewGameControls/Movement.cs
@@ -22,4 +22,33 @@ public class Movement : MonoBehaviour
     {
         transform.Translate(mSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, mSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
     }
+
+
+
+
+
+    IEnumerator  Scheme1()
+    {
+        transform.Translate(mSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, mSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+
+        yield return new WaitForSecondsRealtime(5);
+
+
+        
+        StartCoroutine(Scheme2());
+        StopCoroutine(Scheme1());
+    }
+
+
+    IEnumerator Scheme2()
+    {
+        transform.Translate(mSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, mSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+
+        yield return new WaitForSecondsRealtime(5);
+
+
+        StartCoroutine(Scheme1());
+        StopCoroutine(Scheme2());
+
+    }
 }
